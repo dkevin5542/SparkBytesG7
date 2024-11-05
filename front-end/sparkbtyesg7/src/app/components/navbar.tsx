32fea126da3ef7d440f420d76d725a22e1f0269c
Dana Alzahed <dalzahed@bu.edu>
2024-11-05 17:21:07 -0500

    Initial CSS design updates

--- front-end/sparkbtyesg7/src/app/components/navbar.tsx
+++ front-end/sparkbtyesg7/src/app/components/navbar.tsx
@@ -15,11 +15,11 @@ export const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="navbar-brand" onClick={() => handleNavigation('/')}>Spark Bytes</div>
       <ul className="navbar-links">
-        <li><button onClick={() => handleNavigation('/')}>Home</button></li>
-        <li><button onClick={() => handleNavigation('/about')}>About</button></li> {/* Corrected route */}
-        <li><button onClick={() => handleNavigation('/events')}>Events</button></li>
-        <li><button onClick={() => handleNavigation('/create-event')}>Create Event</button></li>
-        <li><button onClick={() => handleNavigation('/contact')}>Contact</button></li>
+        <li><button onClick={() => handleNavigation('/')}>HOME</button></li>
+        <li><button onClick={() => handleNavigation('/about')}>ABOUT</button></li> {/* Corrected route */}
+        <li><button onClick={() => handleNavigation('/events')}>EVENTS</button></li>
+        <li><button onClick={() => handleNavigation('/create-event')}>CREATE EVENT</button></li>
+        <li><button onClick={() => handleNavigation('/contact')}>CONTACT</button></li>
       </ul>
     </nav>
   );
