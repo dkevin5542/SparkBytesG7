406456fd9e26e1191d3ae0649a419e08791cf30d
jhsualva <jhsualva@bu.edu>
2024-11-05 16:12:13 -0500

    Flask API implementation, added dummy values for database and setup SQLite database

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,36 @@
 # app.py
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 
+# get connection
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# users
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    conn = get_db_connection()
+    users = conn.execute('SELECT * FROM User').fetchall()
+    conn.close()
+    user_list = [dict(row) for row in users]
+    return jsonify(user_list)
+
+# events
+@app.route('/api/events', methods=['GET'])
+def get_events():
+    conn = get_db_connection()
+    events = conn.execute('SELECT * FROM Event').fetchall()
+    conn.close()
+    event_list = [dict(row) for row in events]
+    return jsonify(event_list)
+
+# test for working api
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {
