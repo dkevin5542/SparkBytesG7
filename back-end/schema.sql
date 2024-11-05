a093044035e5e90898078b57d33ebe79e2fd6673
Ming <soongming18@gmail.com>
2024-11-04 23:08:28 -0500

    small changes to schema

--- back-end/schema.sql
+++ back-end/schema.sql
@@ -1,8 +1,8 @@
-create table User(
+CREATE TABLE User(
     user_id INT PRIMARY KEY AUTO_INCREMENT, 
-    email varchar(255) UNIQUE,
+    email varchar(255) UNIQUE NOT NULL,
     diet ENUM('Vegetarian', 'Vegan', 'Omnivore', 'Pescatarian', 'Other') DEFAULT 'Omnivore',
-    preferred_language ENUM('English', 'Mandarin', 'Arabic', 'Spanish')
+    preferred_language ENUM('English', 'Mandarin', 'Arabic', 'Spanish') DEFAULT 'English'
 ); 
 
 CREATE TABLE Event (
@@ -12,7 +12,7 @@ CREATE TABLE Event (
     description TEXT,
     food_type ENUM('Snacks', 'Vegetarian', 'Vegan', 'Gluten-Free', 'Kosher', 'Halal', 'Other') DEFAULT 'Snacks',
     location VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
+    address VARCHAR(255) NOT NULL,
     event_date DATE NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME,
