e2b4b6f4afc88b3384ff90293eb915a9ed6e87ab
jhsualva <jhsualva@bu.edu>
2024-11-12 16:47:08 -0500

    Fixed missing fields in Event dummy values

--- back-end/schema.sql
+++ back-end/schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE Event (
     end_time TIME,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    event_type TEXT CHECK(event_type IN ('Faculty', 'Student')) DEFAULT 'Faculty',
+    event_type TEXT CHECK(event_type IN ('Faculty', 'Student')) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
 );
 
@@ -69,12 +69,12 @@ INSERT INTO User (google_id, email, diet, preferred_language, role) VALUES
     ('daniele_google_id', 'daniele@bu.edu', 'Pescatarian', 'Arabic', 'Faculty'),
     ('evief_google_id', 'evief@bu.edu', 'Other', 'English', 'Faculty');
 
-INSERT INTO Event (user_id, title, description, food_type, food_quantity, location, address, event_date, start_time, end_time) VALUES
-    (1, 'Vegetarian Event', 'A gathering for all vegetarian enthusiasts.', 'Vegetarian', 35, 'College of Arts and Sciences', '725 Commonwealth Ave', '2024-11-01', '12:00:00', '14:00:00'),
-    (2, 'Vegan Lunch', 'Enjoy vegan dishes from around the world.', 'Vegan', 30, 'George Sherman Union', '855 Commonwealth Ave', '2024-11-02', '13:00:00', '15:00:00'),
-    (3, 'Fine Arts Dinner', 'Dinner event with mixed food choices.', 'Other', 50, 'College of Fine Arts', '789 Maple Ave', '2024-11-03', '18:00:00', '20:00:00'),
-    (4, 'BU Beach Picnic', 'Picnic with snacks.', 'Snacks', 17, 'BU Beach', '270 Bay State Road', '2024-11-04', '10:00:00', '12:00:00'),
-    (5, 'Snacks and Books', 'Casual book club meetup with snacks.', 'Snacks', 10, 'Mugar Library', '775 Commonwealth Avenue', '2024-11-05', '15:00:00', '17:00:00');
+INSERT INTO Event (user_id, title, description, food_type, food_quantity, location, address, event_date, start_time, end_time, event_type) VALUES
+    (1, 'Vegetarian Event', 'A gathering for all vegetarian enthusiasts.', 'Vegetarian', 35, 'College of Arts and Sciences', '725 Commonwealth Ave', '2024-11-01', '12:00:00', '14:00:00', 'Faculty'),
+    (2, 'Vegan Lunch', 'Enjoy vegan dishes from around the world.', 'Vegan', 30, 'George Sherman Union', '855 Commonwealth Ave', '2024-11-02', '13:00:00', '15:00:00', 'Faculty'),
+    (3, 'Fine Arts Dinner', 'Dinner event with mixed food choices.', 'Other', 50, 'College of Fine Arts', '789 Maple Ave', '2024-11-03', '18:00:00', '20:00:00', 'Faculty'),
+    (4, 'BU Beach Picnic', 'Picnic with snacks.', 'Snacks', 17, 'BU Beach', '270 Bay State Road', '2024-11-04', '10:00:00', '12:00:00', 'Student'),
+    (5, 'Snacks and Books', 'Casual book club meetup with snacks.', 'Snacks', 10, 'Mugar Library', '775 Commonwealth Avenue', '2024-11-05', '15:00:00', '17:00:00', 'Student');
 
 INSERT INTO Favorite (user_id, event_id) VALUES
     (1, 2),
