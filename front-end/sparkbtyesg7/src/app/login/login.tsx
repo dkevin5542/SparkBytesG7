abc8a41b5b04c6e97f1cad0a56c1f2d03ea7e1b5
jhsualva <jhsualva@bu.edu>
2024-11-12 19:52:31 -0500

    Connected front-end and back-end functionality for Google Login

--- front-end/sparkbtyesg7/src/app/login/login.tsx
+++ front-end/sparkbtyesg7/src/app/login/login.tsx
@@ -7,7 +7,7 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   return (
-    <GoogleOAuthProvider clientId="YOUR_CLIENT_ID_HERE">
+    <GoogleOAuthProvider clientId="140220835320-hp2l5648gotpt7u322qks2eaf7k8ggvn.apps.googleusercontent.com">
       {children}
     </GoogleOAuthProvider>
   );
@@ -18,7 +18,26 @@ export const GoogleAuthButton: React.FC = () => {
     <div className="google-login-button">
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
+          const idToken = credentialResponse.credential;
+
+        fetch('http://localhost:5002/api/google-login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ token: idToken })
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.message === 'Login successful') {
+              console.log('User logged in:', data);
+            } else {
+              console.log('Login failed:', data.message);
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
         }}
         onError={() => {
           console.log('Login Failed');
